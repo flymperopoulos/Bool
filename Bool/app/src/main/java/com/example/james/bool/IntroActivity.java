233992bbfa88354ca7cdd46e7127fe4903d43896
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-11-15 00:07:22 -0500

    initial activity in

--- Bool/app/src/main/java/com/example/james/bool/IntroActivity.java
+++ Bool/app/src/main/java/com/example/james/bool/IntroActivity.java
@@ -1,11 +1,29 @@
 package com.example.james.bool;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * Created by james on 11/14/14.
  */
 public class IntroActivity extends Activity {
-
-
+    @Override
+    protected void onCreate(Bundle FilipposIntro) {
+        super.onCreate(FilipposIntro);
+        setContentView(R.layout.introactivity);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent BeginMain = new Intent("mapkin.mapkin_android.BEGINMAIN");
+                    startActivity(BeginMain);
+                }
+            }
+        };
+        timer.start();
+    }
 }
