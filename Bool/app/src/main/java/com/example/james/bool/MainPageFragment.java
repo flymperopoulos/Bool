874b7a52d0838a6c65eaa4a7d5dedde59bec128d
acejang1994 <byumho.jang@students.olin.edu>
2014-11-15 00:09:06 -0500

    adding listview

--- Bool/app/src/main/java/com/example/james/bool/MainPageFragment.java
+++ Bool/app/src/main/java/com/example/james/bool/MainPageFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by james on 11/14/14.
@@ -16,9 +20,18 @@ public class MainPageFragment extends Fragment {
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-    View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-    return rootView;
-        
+
+        View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+        ListView listViewQuestion = (ListView) rootView.findViewById(R.id.listquestions);
+        ArrayList<String> questions = new ArrayList<String>();
+        questions.add("Do you like Facebook or Google?");
+        questions.add("Do you like Filippos or James?");
+        ArrayAdapter<String> questionAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+        listViewQuestion.setAdapter(questionAdapter);
+
+
+        return rootView;
+
     }
 
 }
